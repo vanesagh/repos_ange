099205679a17e916962ab957bbb4c008bebf00e0
vanesagh <vanesag@chalmers.se>
2023-11-07 18:31:42 -0600

    Added imageUrl attribute

--- src/api/products.js
+++ src/api/products.js
@@ -18,6 +18,7 @@ export const getProducts = async () => {
                 presentation {
                     name
                 }
+                imageUrl
                 
               }
             }
